645dc762743d58a7feed30d5f799d401ad1ac6de
Peter Munn <pcm82@cornell.edu>
2021-04-14 18:18:35 -0400

    modularized search.py

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -1,19 +1,33 @@
 import csv
+import re
 
 
 def top_k(query, k):
     final_data = []
     with open('RAW_recipes_clean.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            url = "https://www.food.com/recipe/" + \
-                row['name'].replace(" ", "-") + "-" + row['id']
-            if query in row['name']:
-                final_data.append("Recipe Name: " + row['name'] + "\n" +
-                                  "Cook Time: " + row['minutes'] + " minutes\n" +
-                                  "Ingredients: " + row['ingredients'] + "\n" +
-                                  "Description: " + row['description'] + "\n" +
-                                  # TODO: format nutrition properly
-                                  "Nutrition: " + row['nutrition'] + "\n" +
-                                  "Recipe Link: " + url)  # TODO: make url a hyperlink
+        data_dict = csv.DictReader(csvfile)
+        for row in find_closest_matches(data_dict, query):
+            final_data.append(format_row(row))
     return final_data[:k]
+
+
+def find_closest_matches(data_dict, query):
+    res = []
+    for row in data_dict:
+        if query in row['name']:
+            res.append(row)
+    return res
+
+
+def format_row(row):
+    recipe_name = re.sub('\s+', '-', row['name'])
+    url = "https://www.food.com/recipe/" + recipe_name.replace(
+        " ", "-") + "-" + row['id']
+    string = str("Recipe Name: " + recipe_name + "\n" +
+                 "Cook Time: " + row['minutes'] + " minutes\n" +
+                 "Ingredients: " + row['ingredients'] + "\n" +
+                 "Description: " + row['description'] + "\n" +
+                 # TODO: format nutrition properly
+                 "Nutrition: " + row['nutrition'] + "\n" +
+                 "Recipe Link: " + url)  # TODO: make url a hyperlink
+    return string
