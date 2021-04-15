7ef1ad2f9a4e32c9971b8a0cf38eb7c3bbaf65b0
Peter Munn <pcm82@cornell.edu>
2021-04-15 16:25:52 -0400

    only output if score > 0

--- app/irsystem/models/search.py
+++ app/irsystem/models/search.py
@@ -22,9 +22,12 @@ def find_closest_matches(data_dict, query):
     # and check that the nutritional information lines up with these keywords
     # for instnace, healthy might mean a ration of 1.2 for protein to fat
     nutritional_dict = {"healthy": 1.2, "low-sugar": 2.0, "high-protein": 5.0}
+    # rank words earlier in the query higher
     for row in data_dict:
         score = 0
-        for token in tokenized_q:
+        for i, token in enumerate(tokenized_q):
+            # since words earlier in the query are worth more, we create an inverse relationship
+            importance_factor = 1/(i + 1)
             if token in row['name']:
                 score += scoring[0]
 
@@ -34,10 +37,11 @@ def find_closest_matches(data_dict, query):
             if token in row['description']:
                 score += scoring[2]
         # TODO: account for nutritional aspects
+        # figure out what good nutritional ratios are, figure out how to read nutrition from our database (what do the labels mean)
             if token in nutritional_dict:
                 pass
-
-        ranked_outputs.append([row, score])
+        if score > 0:
+            ranked_outputs.append([row, score*importance_factor])
 
     # this line sorts the outputs by the score in descending order, then returns a list of only the outputs
     return list(zip(*(sorted(ranked_outputs, key=lambda x: x[1], reverse=True))))[0]
