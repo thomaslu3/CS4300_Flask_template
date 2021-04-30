a8f256e0552459d5baa295c7341310d7f9e9f8b2
Thomas Lu <tal82@cornell.edu>
2021-04-30 12:46:57 -0400

    Added images to output

---
+++ app/irsystem/models/imagescraper.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup as BSHTML
+import urllib.request
+import json
+import re
+import codecs
+
+
+def getImageURL(siteURL):
+    page = urllib.request.urlopen(siteURL)
+    # page = urllib.request.urlopen(siteURL).read().decode('utf-8')
+    soup = BSHTML(page, 'html.parser')
+    # page_text = str(soup.prettify())
+    # decoded_page_text = codecs.decode(page_text, 'unicode-escape')
+    # print(decoded_page_text)
+    # print(page_text)
+    # imageURL = re.search(r"^{\"\@context\":\"http:\/\/schema\.org\".+\"url\":\"https:\/\/www\.food\.com\"\}\}$", decoded_page_text).group(1)
+    # print(imageURL)
+    # with open("output.html", "w", encoding = 'utf-8') as file:
+    # # prettify the soup object and convert it into a string  
+    #     file.write(str(soup.prettify()))
+    images = soup.find_all('meta', {"name" : "og:image"})
+    if(len(images) > 0 and not images[0]['content'] == "https://geniuskitchen.sndimg.com/fdc-new/img/fdc-shareGraphic.png"):
+        return images[0]['content']
+    else:
+        return "../static/images/recipe-placeholder-image.svg"
